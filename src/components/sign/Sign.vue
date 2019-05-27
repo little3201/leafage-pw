849db8937c30a57035705000228012fbcc1c6690
李文强 <liwenqiang@tseverday.com>
2019-05-27 20:10:53 +0800

    modified sign

--- src/components/sign/Sign.vue
+++ src/components/sign/Sign.vue
@@ -1,36 +1,36 @@
 <template>
   <div class="user-login">
-    <div class="user-login-bg"></div>
-    <div class="content-wrapper">
-      <h2 class="slogan">
-        欢迎使用 <br />
-        ICE 内容管理系统
-      </h2>
-      <div class="form-container">
-        <h4 class="form-title">登录</h4>
-        <Form>
-          <div class="form-items">
-            <FormItem prop="user">
-              <Input type="text" v-model="formInline.user" placeholder="请输入用户名">
-                <Icon type="ios-person-outline" slot="prepend" />
-              </Input>
-            </FormItem>
-            <FormItem prop="password">
-              <Input type="password" v-model="formInline.password" placeholder="请输入密码">
-                <Icon type="ios-lock-outline" slot="prepend" />
-              </Input>
-            </FormItem>
-            <FormItem>
-              <Button type="primary" @click="handleSubmit('formInline')">登&nbsp;&nbsp;录</Button>
-            </FormItem>
-          </div>
-        </Form>
-      </div>
-    </div>
+    <div class="user-login-bg">
+			<div class="content-wrapper">
+			  <h2 class="slogan">
+			    欢迎使用 <br />
+			    Abeille 后台管理系统
+			  </h2>
+			  <div class="form-container">
+			    <h2 class="form-title">登&emsp;录</h2>
+			    <Form>
+						<FormItem prop="user">
+							<Input type="text" v-model="formInline.user" placeholder="请输入用户名">
+								<Icon type="ios-person-outline" slot="prepend" />
+							</Input>
+						</FormItem>
+						<FormItem prop="password">
+							<Input type="password" v-model="formInline.password" placeholder="请输入密码">
+								<Icon type="ios-lock-outline" slot="prepend" />
+							</Input>
+						</FormItem>
+						<FormItem>
+								<Button long shape="circle" type="primary" @click="handleSubmit('formInline')">登&emsp;&emsp;录</Button>
+						</FormItem>
+			    </Form>
+			  </div>
+			</div>
+		</div>
   </div>
 </template>
 
 <script>
+import "./Sign.less";
 export default {
   data() {
     return {
@@ -64,14 +64,7 @@ export default {
 </script>
 
 <style>
-@import "./Sign.less";
 .user-login-bg {
   background-image: url("../../assets/laptop.png");
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-size: cover;
 }
 </style>
