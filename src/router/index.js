14e868c37702fc141ff9878f374a19694453d315
李文强 <liwenqiang@tseverday.com>
2019-05-28 19:21:23 +0800

    add some

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,22 @@
 import Vue from "vue";
 import Router from "vue-router";
 import routes from "./routes";
+import { LoadingBar } from "iview";
 
 Vue.use(Router);
+Vue.component('LoadingBar', LoadingBar);
 
 const router = new Router({
   routes,
   mode: "history",
   base: process.env.BASE_URL
 });
+router.beforeEach((to, from, next) => {
+	LoadingBar.start();
+	next();
+});
 
+router.afterEach(route => {
+  LoadingBar.finish();
+});
 export default router;
