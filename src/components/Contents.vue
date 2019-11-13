8fbaf2953a0c9eec6a83d49c21071a63bd7ca7a2
little3201 <little3201@163.com>
2019-11-13 21:15:46 +0800

    add Contents

---
+++ src/components/Contents.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="content">
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Contents extends Vue {}
+</script>
