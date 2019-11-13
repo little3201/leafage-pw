c7ed80a918ab3eca48a5a51a28139c0cecee7839
little3201 <little3201@163.com>
2019-11-13 21:19:04 +0800

    resolve confict

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
   <div class="home">
     <Layout>
@@ -25,34 +24,3 @@
     }
   }
 </script>
-=======
-<template>
-  <div class="home">
-    <Layout>
-      <headers />
-      <Content :style="{ background: '#fff', minHeight: '500px'}">
-        <Card :bordered="false" :style="{ width: '90%', margin: '68px auto 0' }">
-          <div style="text-align:center">
-            <h3>A high quality UI Toolkit based on Vue.js</h3>
-          </div>
-        </Card>
-      </Content>
-      <footers />
-    </Layout>
-  </div>
-</template>
-
-<script>
-// @ is an alias to /src
-import Headers from '@/components/Headers.vue'
-import Footers from '@/components/Footers.vue'
-
-export default {
-  name: 'home',
-  components: {
-    Headers,
-    Footers
-  }
-}
-</script>
->>>>>>> 8c461aa5c38793423e909d21993693ec4bcc2904
