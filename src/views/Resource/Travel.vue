d6c823a2ada310fb748119290011a15b91beef92
little3201 <little3201@163.com>
2019-12-03 23:26:44 +0800

    edit travel.vue

--- src/views/Resource/Travel.vue
+++ src/views/Resource/Travel.vue
@@ -1,13 +1,18 @@
 <template>
-  <el-card :body-style="{ padding: '0px' }">
-    <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
-    <div style="padding: 14px;">
-      <span>好吃的汉堡</span>
-      <div class="bottom clearfix">
-        <el-button type="text" class="button">操作按钮</el-button>
-      </div>
-    </div>
-  </el-card>
+ <el-row :gutter="20" type="flex" justify="center">
+    <el-col v-for="index in 3" :key="index">
+      <el-card :xs="24" :sm="16" :md="15" :lg="14" :body-style="{ padding: '0px' }">
+        <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
+        <div style="padding: 14px;">
+          <span>好吃的汉堡</span>
+          <div class="bottom clearfix">
+            <time class="time">{{ currentDate }}</time>
+            <el-button type="text" class="button">操作按钮</el-button>
+          </div>
+        </div>
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
