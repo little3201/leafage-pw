672a619f3b1bfe085e114202055fd34c35d95e1a
little3201 <little3201@163.com>
2020-02-12 14:02:55 +0800

    edit Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,10 @@
     <v-app-bar
       :clipped-left="$vuetify.breakpoint.lgAndUp"
       app
-      color="blue darken-3"
-      dark
+      color="white"
+      light
+      flat
+      inverted-scroll
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="hidden-md-and-up"/>
       <v-toolbar-title
@@ -15,49 +17,144 @@
         </router-link>
       </v-toolbar-title>
       <v-spacer />
-      <v-text-field
-        flat
-        solo-inverted
-        hide-details
-        prepend-inner-icon="mdi-magnify"
-        label="Search"
-        class="hidden-sm-and-down"
-        rounded
-        dense
-      />
-      <v-spacer />
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn href="/" text>首页</v-btn>
-        <v-menu offset-y>
-          <template v-slot:activator="{ on }">
-            <v-btn v-on="on" text style="padding-right: 0;">
-              博客<v-icon>mdi-menu-down</v-icon>
-            </v-btn>
-          </template>
-          <v-list>
-            <v-list-item
-              v-for="(item, index) in menus"
-              :key="index"
-              :href="item.url"
-            >
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-        <v-btn href="/introduce" text>介绍</v-btn>
+        <v-btn href="/blog" text>博客系统</v-btn>
+        <v-btn href="/resource" text>资源</v-btn>
         <v-btn href="/" text>帮助</v-btn>
-        <v-btn href="/signin" text>登录</v-btn>
-        <v-btn href="/signup" text>注册</v-btn>
       </v-toolbar-items>
     </v-app-bar>
     <v-content>
       <v-container
         fluid
       >
-        <!-- If using vue-router -->
-        <v-row justify="center" class="my-0" outlined hover>
+        <v-img src="../assets/wechat.jpg"></v-img>
+        <v-row justify="center" class="my-0">
+          <v-col cols="12" sm="6" md="4" lg="3" xl="3">
+            <v-card
+              class="mx-auto"
+              max-width="400"
+              outlined
+              hover
+            >
+              <v-img
+                class="white--text align-end"
+                height="200px"
+                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+              >
+                <v-card-title>Top 10 Australian beaches</v-card-title>
+              </v-img>
+
+              <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+
+              <v-card-text class="text--primary">
+                <div>Whitehaven Beach</div>
+
+                <div>Whitsunday Island, Whitsunday Islands</div>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn
+                  color="orange"
+                  text
+                >
+                  Share
+                </v-btn>
+
+                <v-btn
+                  color="orange"
+                  text
+                >
+                  Explore
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="6" md="4" lg="3" xl="3">
+            <v-card
+              class="mx-auto"
+              max-width="400"
+              outlined
+              hover
+            >
+              <v-img
+                class="white--text align-end"
+                height="200px"
+                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+              >
+                <v-card-title>Top 10 Australian beaches</v-card-title>
+              </v-img>
+
+              <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+
+              <v-card-text class="text--primary">
+                <div>Whitehaven Beach</div>
+
+                <div>Whitsunday Island, Whitsunday Islands</div>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn
+                  color="orange"
+                  text
+                >
+                  Share
+                </v-btn>
+
+                <v-btn
+                  color="orange"
+                  text
+                >
+                  Explore
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="6" md="4" lg="3" xl="3">
+            <v-card
+              class="mx-auto"
+              max-width="400"
+              outlined
+              hover
+            >
+              <v-img
+                class="white--text align-end"
+                height="200px"
+                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+              >
+                <v-card-title>Top 10 Australian beaches</v-card-title>
+              </v-img>
+
+              <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+
+              <v-card-text class="text--primary">
+                <div>Whitehaven Beach</div>
+
+                <div>Whitsunday Island, Whitsunday Islands</div>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn
+                  color="orange"
+                  text
+                >
+                  Share
+                </v-btn>
+
+                <v-btn
+                  color="orange"
+                  text
+                >
+                  Explore
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+        <!-- chart -->
+        <v-row justify="center" class="my-0">
           <v-col cols="12" md="10" lg="10" xl="8">
-            <v-card class="pa-7">
+            <v-card class="pa-7" outlined>
               <v-sparkline
                 :value="value"
                 :gradient="gradient"
@@ -76,6 +173,37 @@
         </v-row>
       </v-container>
     </v-content>
+    <v-footer
+    dark
+    padless
+  >
+    <v-card
+      flat
+      tile
+      class="indigo lighten-1 white--text text-center"
+    >
+      <v-card-text>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 white--text"
+          icon
+        >
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-card-text class="white--text pt-0">
+        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
   </v-app>
 </template>
 
@@ -104,21 +232,7 @@ export default {
     type: 'trend',
     autoLineWidth: false,
     dialog: false,
-    drawer: null,
-    menus: [
-      {
-        title: '技术博文',
-        url: '/technology'
-      },
-      {
-        title: '翻译文档',
-        url: '/translation'
-      },
-      {
-        title: '摄影记录',
-        url: '/travel'
-      }
-    ]
+    drawer: null
   })
 }
 </script>
