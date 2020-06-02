0810984b0f0335888b5a80e541dfc20a14d2966c
李文强 <liwenqiang@tseverday.com>
2020-06-02 11:33:22 +0800

    load-sections修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,9 @@ module.exports = {
 
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'template-curly-spacing': 'off',
+    indent: 'off'
   },
 
   parserOptions: {
