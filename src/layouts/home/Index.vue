652546ce680e8d99b0783e59d6dceef193e5dfbc
李文强 <liwenqiang@tseverday.com>
2020-06-05 18:12:08 +0800

    去掉webfont依赖

--- src/layouts/home/Index.vue
+++ src/layouts/home/Index.vue
@@ -20,7 +20,6 @@ export default {
     HomeAppBar: () => import('@/layouts/home/AppBar'),
     HomeFooter: () => import('@/layouts/home/Footer'),
     HomeSettings: () => import('@/layouts/home/Settings'),
-    HomeSystemBar: () => import('@/layouts/home/SystemBar'),
     HomeView: () => import('@/layouts/home/View')
   }
 }
