e822f1f10810d8319a80cdfca0a42656c215d112
李文强 <liwenqiang@tseverday.com>
2020-06-06 15:12:47 +0800

    添加meta

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -8,7 +8,13 @@ import LoadSections from '@/mixins/load-sections'
 export default {
   name: 'Home',
 
-  metaInfo: { title: 'Home' },
+  metaInfo: { 
+    title: 'Home',
+    meta: [
+      { name: 'keywords', content: '网站由布吉岛开发维护' },
+      { name: 'decoration', content: '一套免费、开源的前后端服务产品、包括精美的前端展示、后端运营和完整的后端服务，更惊喜的是，这一切都是免费的，并且长期维护的！' }
+    ]
+  },
 
   extends: View,
 
