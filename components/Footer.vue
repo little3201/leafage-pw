e5aac3de6dae9532fccebc1720e2f26080cf2ce6
little3201 <little3201@163.com>
2020-12-11 18:03:25 +0800

    footer使用tailwindcss重构

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,128 +1,95 @@
 <template>
-  <footer>
-    <div class="container">
-      <div class="blog-items ft-style p-70">
-        <div class="row">
-          <div class="col-lg-3 col-md-3 col-sm-6 col-12">
-            <div class="blog-item">
-              <div class="blog-img">
-                <img src="images/resources/ft-img1.jpg" alt="" />
-              </div>
-              <!--blog-img end-->
-              <div class="blog-info">
-                <h3 class="post-title">
-                  <a href="standard-post.html" title=""
-                    >Five Quotes For Some Extra Monday Motivation</a
-                  >
-                </h3>
-              </div>
-              <!--blog-info end-->
-            </div>
-            <!--blog-item end-->
-          </div>
-          <div class="col-lg-3 col-md-3 col-sm-6 col-12">
-            <div class="blog-item">
-              <div class="blog-img">
-                <img src="images/resources/ft-img2.jpg" alt="" />
-              </div>
-              <!--blog-img end-->
-              <div class="blog-info">
-                <h3 class="post-title">
-                  <a href="standard-post.html" title=""
-                    >The No Sugar Challenge That Almost Broke Me</a
-                  >
-                </h3>
-              </div>
-              <!--blog-info end-->
-            </div>
-            <!--blog-item end-->
-          </div>
-          <div class="col-lg-3 col-md-3 col-sm-6 col-12">
-            <div class="blog-item">
-              <div class="blog-img">
-                <img src="images/resources/ft-img3.jpg" alt="" />
-              </div>
-              <!--blog-img end-->
-              <div class="blog-info">
-                <h3 class="post-title">
-                  <a href="standard-post.html" title=""
-                    >Ways To Make More Time in Motherhood Career</a
-                  >
-                </h3>
-              </div>
-              <!--blog-info end-->
-            </div>
-            <!--blog-item end-->
-          </div>
-          <div class="col-lg-3 col-md-3 col-sm-6 col-12">
-            <div class="blog-item">
-              <div class="blog-img">
-                <img src="images/resources/ft-img4.jpg" alt="" />
-              </div>
-              <!--blog-img end-->
-              <div class="blog-info">
-                <h3 class="post-title">
-                  <a href="standard-post.html" title=""
-                    >Lessons I’ve Learned From Being A Mom</a
-                  >
-                </h3>
-              </div>
-              <!--blog-info end-->
-            </div>
-            <!--blog-item end-->
-          </div>
+  <footer class="bg-gray-900">
+    <div class="container mx-auto">
+      <div class="flex justify-between items-center text-white h-32">
+        <div class="text-4xl tracking-widest font-black uppercase">
+          <h1><a href="#" title="">Heeney</a></h1>
+        </div>
+        <div class="flex space-x-6">
+          <a href="#" title=""
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-facebook"
+            >
+              <path
+                d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"
+              ></path></svg
+          ></a>
+          <a href="#" title=""
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-twitter"
+            >
+              <path
+                d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
+              ></path></svg
+          ></a>
+          <a href="#" title=""
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-linkedin"
+            >
+              <path
+                d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
+              ></path>
+              <rect x="2" y="9" width="4" height="12"></rect>
+              <circle cx="4" cy="4" r="2"></circle></svg
+          ></a>
+          <button class="rounded-md"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-github"
+            >
+              <path
+                d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+              ></path></svg
+          ></button>
+        </div>
+        <div class="flex space-x-6 uppercase text-xs font-extrabold ">
+          <nuxt-link to="/about">About</nuxt-link>
+          <nuxt-link to="/contact">Contact</nuxt-link>
+          <!--ft-links end-->
         </div>
       </div>
-      <!--blog-items end-->
-      <div class="footer-content">
-        <div class="row">
-          <div class="col-lg-4 col-md-6 col-sm-6">
-            <div class="ft-logo">
-              <h1><a href="#" title="">Heeney</a></h1>
-            </div>
-            <!--ft-logo end-->
-          </div>
-          <div class="col-lg-4 col-md-6 col-sm-6">
-            <ul class="social-links">
-              <li>
-                <a href="#" title=""><i class="fab fa-facebook-f"></i></a>
-              </li>
-              <li>
-                <a href="#" title=""><i class="fab fa-twitter"></i></a>
-              </li>
-              <li>
-                <a href="#" title=""><i class="fab fa-linkedin-in"></i></a>
-              </li>
-              <li>
-                <a href="#" title=""><i class="fab fa-pinterest-p"></i></a>
-              </li>
-              <li>
-                <a href="#" title=""
-                  ><i class="fab fa-facebook-messenger"></i
-                ></a>
-              </li>
-            </ul>
-          </div>
-          <div class="col-lg-4 col-md-12">
-            <ul class="ft-links">
-              <li><a href="about.html" title="">About</a></li>
-              <li><a href="contact.html" title="">Contact</a></li>
-            </ul>
-            <!--ft-links end-->
-          </div>
-        </div>
-      </div>
-      <!--footer-content end-->
     </div>
-    <section class="bottom-strip">
-      <div class="container">
-        <p>
-          &copy; {{ new Date().getFullYear() }} Abeille. All Rights
-          <a href="http://www.bootstrapmb.com/" title="bootstrapmb">Reserved</a
-          >.
-        </p>
-        <p>陕ICP备19017836号-1</p>
-      </div>
+    <section class="bg-black py-2 text-xs text-center text-gray-500">
+      <p>
+        &copy; {{ new Date().getFullYear() }} Abeille. All Rights
+        <a href="http://www.bootstrapmb.com/" title="bootstrapmb">Reserved</a>.
+      </p>
+      <p>陕ICP备19017836号-1</p>
     </section>
   </footer>
 </template>
