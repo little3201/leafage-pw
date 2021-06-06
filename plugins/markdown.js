b20e50c6141432db8f779b377c572fe392753547
liwenqiang <little3201@163.com>
2021-06-06 20:07:03 +0800

    highlight.js语言库引入更新

--- plugins/markdown.js
+++ plugins/markdown.js
@@ -6,12 +6,12 @@ import bash from 'highlight.js/lib/languages/bash'
 import javascript from 'highlight.js/lib/languages/javascript';
 import typescript from 'highlight.js/lib/languages/typescript';
 import java from 'highlight.js/lib/languages/java';
-import sql from 'highlight.js/lib/languages/sql_more';
+import sql from 'highlight.js/lib/languages/sql';
 import nginx from 'highlight.js/lib/languages/nginx';
 import json from 'highlight.js/lib/languages/json';
 import yaml from 'highlight.js/lib/languages/yaml';
 import xml from 'highlight.js/lib/languages/xml';
-import htmlbars from 'highlight.js/lib/languages/htmlbars'
+import handlebars from 'highlight.js/lib/languages/handlebars'
 import shell from 'highlight.js/lib/languages/shell'
 import dockerfile from 'highlight.js/lib/languages/dockerfile'
 
@@ -24,7 +24,7 @@ hljs.registerLanguage('nginx', nginx);
 hljs.registerLanguage('json', json);
 hljs.registerLanguage('yaml', yaml);
 hljs.registerLanguage('xml', xml);
-hljs.registerLanguage('htmlbars', htmlbars);
+hljs.registerLanguage('handlebars', handlebars);
 hljs.registerLanguage('shell', shell);
 hljs.registerLanguage('dockerfile', dockerfile);
 
