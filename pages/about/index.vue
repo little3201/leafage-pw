299668a9e7989e98bd587f38888edc6776471374
liwenqiang <little3201@163.com>
2021-09-20 17:39:49 +0800

    about.vue完成，内容待补充

---
+++ pages/about/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>
+            说起来是从18年开始有了这个想法———做一个自己的网站，风格清新，内容新颖，干货满满。我是一个后端开发，想要一个界面美观、
+            布局合理的网站可是不容易，于是开始动手，网站先后改版、重构了四五次了，最终呈现现在这个结果，还算满意。
+        </p>
+        <p class="my-2">
+            在这个漫长的折腾的过程中，用过element-ui、ant-design-vue、iview(viewui)、vuetify等组件库，试过整屏翻页，
+            模仿过轻芒杂志，也试过vuetify的免费模版，但是这些组件库都很重，最终选择了tailwindcss，seo优化选择了nuxtjs来做。
+        </p>
+        <p>
+            与此对应的还有
+            <a href="https://console.leafage.top" target="_blank" rel="noopener">后台管理</a>开发，
+            也全面改用tailwindcss来做，顺势而为选择了vue3进行重构，更多功能还在持续优化和完善中，敬请期待。
+        </p>
+    </div>
+</template>
