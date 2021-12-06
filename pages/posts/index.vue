7743a1c37b4979a51ae2fbcc1149a969def78de6
little3201 <little3201@163.com>
2021-12-06 19:26:16 +0800

    tab切换数据不刷新问题解决

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <Tab @chageParams="chageParams" :datas="categories" />
-    <div class="grid grid-cols-1 gap-y-8 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 my-8">
+    <div
+      class="grid grid-cols-1 gap-y-8 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 my-8"
+    >
       <Item v-for="data in datas" :key="data.code" :data="data" />
     </div>
     <Pagation :page="page" :size="size" :total="total" @retrieve="chageParams" />
@@ -34,7 +36,7 @@ export default {
   data() {
     return {
       page: 0,
-      size:  16
+      size: 16
     }
   },
 
@@ -47,9 +49,9 @@ export default {
     async retrieve(code) {
       await Promise.all([
         this.$axios.$get(SERVER_URL.posts, {
-          params: { page: this.page, size: this.size, category: code },
-        }),
-        this.$axios.$get(SERVER_URL.posts.concat("/count"))])
+          params: { page: this.page, size: this.size, ca: code },
+        }).then(res => this.datas = res.data),
+        this.$axios.$get(SERVER_URL.posts.concat("/count").then(res => this.total = res.data))])
     }
   }
 }
