c2bafafe1461b81bb5f490982d4a9112370c28e8
little3201 <little3201@163.com>
2021-12-09 20:29:55 +0800

    添加评论模块

--- pages/posts/detail/_slug.vue
+++ pages/posts/detail/_slug.vue
@@ -80,12 +80,69 @@
         v-html="rendered"
       ></article>
 
-      <div class="inline-flex items-center space-x-2 mt-4">
-        <span v-for="(tag, index) in data.tags" :key="index" class="text-xs bg-gray-200 rounded-md px-2 py-1">
-          {{ "#" + tag }}
-        </span>
+      <div class="inline-flex items-center space-x-2 mt-8">
+        <span
+          v-for="(tag, index) in data.tags"
+          :key="index"
+          class="text-xs bg-gray-200 dark:bg-gray-600 rounded-md px-2 py-1"
+        >{{ "#" + tag }}</span>
       </div>
-      <div class="bg-gray-100 dark:bg-gray-600 my-8 p-6">
+
+      <div class="flex items-center justify-center space-x-8 my-6">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-thumbs-up cursor-pointer"
+        >
+          <path
+            d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"
+          />
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-heart cursor-pointer"
+        >
+          <path
+            d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
+          />
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-share-2 cursor-pointer"
+        >
+          <circle cx="18" cy="5" r="3" />
+          <circle cx="6" cy="12" r="3" />
+          <circle cx="18" cy="19" r="3" />
+          <line x1="8.59" y1="13.51" x2="15.42" y2="17.49" />
+          <line x1="15.41" y1="6.51" x2="8.59" y2="10.49" />
+        </svg>
+      </div>
+      <div class="bg-gray-100 dark:bg-gray-600 dark:text-gray-300 my-8 p-6">
         <ul
           class="grid grid-flow-row grid-rows-2 grid-cols-1 md:grid-rows-1 md:grid-cols-2 gap-4 text-sm font-bold"
         >
@@ -139,7 +196,43 @@
           </li>
         </ul>
       </div>
-      <Comment v-for="comment in comments" :key="comment.code" :data="comment" />
+      <div>
+        <form class="my-6">
+          <div class="items-center -mx-2 md:flex">
+            <div class="w-full mx-2">
+              <label class="text-sm font-medium text-gray-600 dark:text-gray-200">Name</label>
+              <input
+                class="w-full mt-1 px-4 py-2 border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:outline-none focus:ring-1 focus:ring-gray-400"
+                type="text"
+              />
+            </div>
+
+            <div class="w-full mx-2 mt-4 md:mt-0">
+              <label class="text-sm font-medium text-gray-600 dark:text-gray-200">E-mail</label>
+              <input
+                class="w-full mt-1 px-4 py-2 border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:outline-none focus:ring-1 focus:ring-gray-400"
+                type="email"
+              />
+            </div>
+          </div>
+
+          <div class="w-full mt-4">
+            <label class="text-sm font-medium text-gray-600 dark:text-gray-200">Message</label>
+            <textarea
+              class="w-full mt-1 px-4 py-2 border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:outline-none focus:ring-1 focus:ring-gray-400"
+            ></textarea>
+          </div>
+
+          <div class="flex justify-end mt-6">
+            <button
+              @click="onsubmit"
+              class="px-4 py-2 transition-colors duration-200 transform border border-gray-300 dark:bg-gray-600 bg-gray-100 rounded-md hover:bg-gray-300 focus:outline-none"
+            >Send Message</button>
+          </div>
+        </form>
+
+        <Comment v-for="comment in comments" :key="comment.code" :data="comment" />
+      </div>
     </div>
     <LazyAside class="my-8" />
   </div>
@@ -193,6 +286,29 @@ export default {
     let rendered = markdown.render(data.content);
 
     return { data, previous, next, comments, rendered }
+  },
+
+  data() {
+    return {
+      formData: {}
+    }
+  },
+
+  methods: {
+    /**
+     * 评论提交
+     */
+    onsubmit() {
+      this.$axios.post(SERVER_URL.comment.concat("/", this.formData))
+    },
+
+    /**
+     * 点赞
+     */
+    likes(code) {
+      this.$axios.patch(SERVER_URL.posts.concat("/", code))
+    }
+
   }
 }
 </script>
