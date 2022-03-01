b22accc905e9b0632f3eb2ddd9d3fd59bd145196
little3201 <little3201@163.com>
2022-03-01 18:55:02 +0800

    更新ga.client.js配置

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    { src: '~/plugins/ga.client', ssr: false },
+    { src: '~/plugins/ga.client', mode: 'client' },
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
