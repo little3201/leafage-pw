a6fd046cf24180fbc4a12da32f201cec20311dce
little3201 <little3201@163.com>
2022-03-14 08:43:53 +0800

    设置about 内容最小高度

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
             <LayoutHeader class="flex border-gray-900 dark:border-gray-300 border-b-2" />
             <LayoutLogo />
         </header>
-        <main class="max-w-2xl mx-auto px-4 sm:px-6 lg:max-w-5xl lg:px-12">
+        <main class="max-w-2xl mx-auto px-4 sm:px-6 lg:max-w-5xl lg:px-12" style="min-height: calc(100vh - 23rem);">
             <slot />
         </main>
         <LayoutFooter />
