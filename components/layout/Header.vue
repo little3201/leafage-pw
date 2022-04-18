d43a9a81f922abc0ae332e988796d34e49ec0ad8
little3201 <little3201@163.com>
2022-04-18 11:25:54 +0800

    更新标签切换过程，数据不覆盖的问题

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,75 +1,30 @@
 <template>
-    <div
-        class="flex justify-between items-center mx-4 md:mx-0 py-3 md:py-5 text-gray-900 dark:text-gray-300"
-    >
-        <button
-            @click="isShow = !isShow"
-            type="button"
-            aria-label="openMenu"
-            class="focus:outline-none md:hidden"
-        >
-            <svg
-                width="24"
-                height="24"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-                aria-hidden="true"
-            >
-                <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                />
+    <div class="flex justify-between items-center mx-4 md:mx-0 py-3 md:py-5 text-gray-900 dark:text-gray-300">
+        <button @click="isShow = !isShow" type="button" aria-label="openMenu" class="focus:outline-none md:hidden">
+            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor" stroke-width="2" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
         </button>
         <Drawer :isShow="isShow" @closeAction="drawerOperation" />
 
-        <nav
-            class="hidden md:inline-flex items-center space-x-6 tracking-wide font-semibold text-xs uppercase"
-        >
+        <nav class="hidden md:inline-flex items-center space-x-6 tracking-wide font-semibold text-xs uppercase">
             <NuxtLink title="home" to="/">Home</NuxtLink>
             <NuxtLink title="posts" to="/posts">Posts</NuxtLink>
             <NuxtLink title="resource" to="/resource">Resource</NuxtLink>
             <NuxtLink title="about" to="/about">About</NuxtLink>
         </nav>
         <div class="inline-flex items-center">
-            <button
-                title="Toggle Theme"
-                @click="theme"
-                class="hidden md:block relative focus:outline-none transition-colors duration-500 ease-in border-transparent"
-            >
-                <svg
-                    v-if="isDark"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="feather feather-moon"
-                >
+            <button title="Toggle Theme" @click="theme"
+                class="hidden md:block relative focus:outline-none transition-colors duration-500 ease-in border-transparent">
+                <svg v-if="isDark" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"
+                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="feather feather-moon">
                     <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
                 </svg>
-                <svg
-                    v-else
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="feather feather-sun"
-                >
+                <svg v-else xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="feather feather-sun">
                     <circle cx="12" cy="12" r="5" />
                     <line x1="12" y1="1" x2="12" y2="3" />
                     <line x1="12" y1="21" x2="12" y2="23" />
@@ -82,52 +37,21 @@
                 </svg>
             </button>
 
-            <a
-                title="contact me"
-                class="hidden md:flex items-center uppercase text-xs font-bold tracking-wide mx-6"
-                href="mailto:little3201@163.com"
-                rel="noopener"
-                aria-label="contact me"
-            >
-                <svg
-                    style="display: unset"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="feather feather-mail mr-2"
-                >
-                    <path
-                        d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"
-                    />
+            <a title="contact me" class="hidden md:flex items-center uppercase text-xs font-bold tracking-wide mx-6"
+                href="mailto:little3201@163.com" rel="noopener" aria-label="contact me">
+                <svg style="display: unset" xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                    stroke-linejoin="round" class="feather feather-mail mr-2">
+                    <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
                     <polyline points="22,6 12,13 2,6" />
                 </svg>
                 Contact Me
             </a>
-            <button
-                type="button"
-                title="search"
-                class="focus:outline-none"
-                aria-label="searchOpen"
-                @click="isSearch = !isSearch"
-            >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="feather feather-search"
-                >
+            <button type="button" title="search" class="focus:outline-none" aria-label="searchOpen"
+                @click="isSearch = !isSearch">
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="feather feather-search">
                     <circle cx="11" cy="11" r="8" />
                     <line x1="21" y1="21" x2="16.65" y2="16.65" />
                 </svg>
@@ -138,6 +62,8 @@
 
 <script>
 import Drawer from './Drawer.vue'
+
+
 export default {
     name: "Header",
 
@@ -149,7 +75,8 @@ export default {
         return {
             isDark: false,
             isSearch: false,
-            isShow: false
+            isShow: false,
+            isSearch: false
         };
     },
 
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,11 +2,9 @@
     <div>
         <p>
             项目中的接口可以直接替换为自己的服务接口，后端服务：
-            <a
-                href="https://github.com/little3201/leafage-basic"
-                target="_blank"
-            >leafage-basic</a>，希望大家使用本项目的同时也能一起学习，分享。另外有一个
-            <a href="https://console.leafage.top" target="_blank">后台管理</a> 系统。
+            <a href="https://github.com/little3201/leafage-basic" target="_blank"
+                rel=noopener>leafage-basic</a>，希望大家使用本项目的同时也能一起学习，分享。另外有一个
+            <a href="https://console.leafage.top" target="_blank" rel=noopener>后台管理</a> 系统。
         </p>
         <p class="text-base">
             项目开发秉持整洁、高质量、规范的代码，尽可能的减少代码来实现功能；项目所有样式设置均直接使用 tailwindcss 直接在标签上进性设置，
