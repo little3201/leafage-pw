472c42ed72b0d29517d3705933a62768d0877f09
little3201 <little3201@163.com>
2022-04-18 10:44:27 +0800

    替换ga为gata

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,12 +22,12 @@ export default {
 
   // Global CSS
   css: [
-    '@/assets/css/main.css'
+    '~/assets/css/main.css'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    { src: '~/plugins/ga.client', mode: 'client' },
+    '~/plugins/gata.client'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
