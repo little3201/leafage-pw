26f6b10224ea7ed37c24c2bf99c8c112e2474312
liwenqiang <little3201@163.com>
2022-10-26 19:39:27 +0800

    开启swcMinify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['cdn.leafage.top', 'secure.gravatar.com'],
     formats: ['image/avif', 'image/webp'],
