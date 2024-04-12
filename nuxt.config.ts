95fba3f27923d6876b486ae893615425222f6222
liwenqiang <little3201@163.com>
2024-04-12 15:14:00 +0800

    config with yarn pnp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['@nuxt/ui-pro'],
   modules: [
     '@nuxt/content',
-    '@nuxt/ui',
     '@nuxt/image',
     '@nuxt/fonts',
     '@vueuse/nuxt'
   ],
-  hooks: {
-    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
-    'components:extend': (components) => {
-      const globals = components.filter((c) => ['UButton'].includes(c.pascalName))
-
-      globals.forEach((c) => c.global = true)
-    }
-  },
-  ui: {
-    icons: ['heroicons', 'simple-icons']
-  },
   routeRules: {
     '/api/search.json': { prerender: true },
     '/docs': { redirect: '/docs/getting-started', prerender: false }
